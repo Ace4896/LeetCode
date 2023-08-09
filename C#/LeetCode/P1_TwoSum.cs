cd77d036404174e98b8b6d55e9b2986c7bf321b4
Jon Pacheco <jon.pacheco@tutanota.com>
2023-08-09 23:37:34 +0100

    feat: add solutions to 1 - two sum

---
+++ C#/LeetCode/P1_TwoSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode;
+
+public class P1_TwoSum
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        // Naive Solution: O(n^2)
+        //for (int first = 0; first < nums.Length - 1; first++)
+        //{
+        //    for (int last = first + 1; last < nums.Length; last++)
+        //    {
+        //        if (nums[first] + nums[last] == target)
+        //        {
+        //            return new int[] { first, last };
+        //        }
+        //    }
+        //}
+
+        // Dictionary Solution: O(n)
+        Dictionary<int, int> indices = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (indices.TryGetValue(complement, out int complementIndex))
+            {
+                return new int[] { complementIndex, i };
+            }
+
+            indices[nums[i]] = i;
+        }
+
+        throw new InvalidOperationException("No solution");
+    }
+}
