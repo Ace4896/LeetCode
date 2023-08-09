49b7b82c1f423a6b2f727a92620e9d05bb4b2de6
Jon Pacheco <jon.pacheco@tutanota.com>
2023-08-10 00:00:50 +0100

    feat: add C# solution to 20 - valid parentheses

---
+++ C#/LeetCode/P20_ValidParentheses.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode;
+
+public class P20_ValidParentheses
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> openParentheses = new();
+        char openParenthesis;
+
+        foreach (char c in s)
+        {
+            switch (c)
+            {
+                case '(':
+                case '[':
+                case '{':
+                    openParentheses.Push(c);
+                    break;
+
+                case ')':
+                    if (!openParentheses.TryPop(out openParenthesis) || openParenthesis != '(')
+                    {
+                        return false;
+                    }
+
+                    break;
+
+                case ']':
+                    if (!openParentheses.TryPop(out openParenthesis) || openParenthesis != '[')
+                    {
+                        return false;
+                    }
+
+                    break;
+
+                case '}':
+                    if (!openParentheses.TryPop(out openParenthesis) || openParenthesis != '{')
+                    {
+                        return false;
+                    }
+
+                    break;
+            }
+        }
+
+        return openParentheses.Count == 0;
+    }
+}
