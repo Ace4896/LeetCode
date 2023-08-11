71b0a8f784c6dadc7c30f82578f9913f6aeb921f
Jon Pacheco <jon.pacheco@tutanota.com>
2023-08-11 11:06:35 +0100

    feat: add ms sql solution to 175 - combine two tables

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This repository contains my solutions to various LeetCode challenges.
 ## Requirements
 
 - C#: .NET 6.0
+- SQL: N/A, but queries are written using MS SQL Server's dialect.
---
+++ SQL/175 - Combine Two Tables.sql
@@ -0,0 +1,5 @@
+-- Report the first name, last name, city and state of each person in the 'Person' table.
+-- If the address of a 'personId' is not present in the 'Address' table, report 'null' instead.
+SELECT firstName, lastName, city, state
+FROM Person
+LEFT JOIN Address on Person.personId = Address.personId;
